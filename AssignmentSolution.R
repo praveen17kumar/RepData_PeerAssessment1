6e4fc06694df1574f8c9c43cd12c4b6bb29e8cc2
praveen <pklappy21@gmail.com>
2018-10-04 06:13:15 +0530

    commit 2

--- AssignmentSolution.R
+++ AssignmentSolution.R
@@ -9,3 +9,14 @@ activity$DateTime<- as.POSIXct(activity$date, format="%Y-%m-%d")
 ##pulling data without NA's
 clean <- activity[!is.na(activity$steps),]
 
+## summarizing total steps per date
+sumTable <- aggregate(activity$steps ~ activity$date, FUN=sum, )
+colnames(sumTable)<- c("Date", "Steps")
+
+## Creating the historgram of total steps per day
+hist(sumTable$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day")
+
+## Mean of Steps
+as.integer(mean(sumTable$Steps))
+## Median of Steps
+as.integer(median(sumTable$Steps))
