91b8bc551ec884c944f8dbaddc3666f291da0179
praveen <pklappy21@gmail.com>
2018-10-04 06:23:27 +0530

    commit 5

--- AssignmentSolution.R
+++ AssignmentSolution.R
@@ -33,3 +33,38 @@ p + geom_line()+xlab("Interval")+ylab("Average Number of Steps")+ggtitle("Averag
 maxSteps <- max(intervalTable$Avg)
 ##Which interval contains the maximum average number of steps
 intervalTable[intervalTable$Avg==maxSteps,1]
+
+
+##Number of NAs in original data set
+nrow(activity[is.na(activity$steps),])
+
+## Create the average number of steps per weekday and interval
+avgTable <- ddply(clean, .(interval, day), summarize, Avg = mean(steps))
+
+## Create dataset with all NAs for substitution
+nadata<- activity[is.na(activity$steps),]
+## Merge NA data with average weekday interval for substitution
+newdata<-merge(nadata, avgTable, by=c("interval", "day"))
+
+## Reorder the new substituded data in the same format as clean data set
+newdata2<- newdata[,c(6,4,1,2,5)]
+colnames(newdata2)<- c("steps", "date", "interval", "day", "DateTime")
+
+##Merge the NA averages and non NA data together
+mergeData <- rbind(clean, newdata2)
+
+##Create sum of steps per date to compare with step 1
+sumTable2 <- aggregate(mergeData$steps ~ mergeData$date, FUN=sum )
+colnames(sumTable2)<- c("Date", "Steps")
+
+## Mean of Steps with NA data taken care of
+as.integer(mean(sumTable2$Steps))
+
+## Median of Steps with NA data taken care of
+as.integer(median(sumTable2$Steps))
+
+## Creating the histogram of total steps per day, categorized by data set to show impact
+hist(sumTable2$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day with NAs Fixed", col="Black")
+hist(sumTable$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day with NAs Fixed", col="Grey", add=T)
+legend("topright", c("Imputed Data", "Non-NA Data"), fill=c("black", "grey") )
+
