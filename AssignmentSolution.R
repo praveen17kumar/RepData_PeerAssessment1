ab392de1fe44d7ab2526f90f0a70ded38130e500
praveen <pklappy21@gmail.com>
2018-10-04 06:24:39 +0530

    commit 6

--- AssignmentSolution.R
+++ AssignmentSolution.R
@@ -68,3 +68,15 @@ hist(sumTable2$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day with N
 hist(sumTable$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day with NAs Fixed", col="Grey", add=T)
 legend("topright", c("Imputed Data", "Non-NA Data"), fill=c("black", "grey") )
 
+## Create new category based on the days of the week
+mergeData$DayCategory <- ifelse(mergeData$day %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
+
+library(lattice) 
+
+## Summarize data by interval and type of day
+intervalTable2 <- ddply(mergeData, .(interval, DayCategory), summarize, Avg = mean(steps))
+
+##Plot data in a panel plot
+xyplot(Avg~interval|DayCategory, data=intervalTable2, type="l",  layout = c(1,2),
+       main="Average Steps per Interval Based on Type of Day", 
+       ylab="Average Number of Steps", xlab="Interval")
